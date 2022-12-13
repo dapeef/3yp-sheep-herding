5c9505d111525f38468e87436b2c6f73ff9cba3f
dapeef <42572235+dapeef@users.noreply.github.com>
2022-12-13 13:08:44 +0000

    Add first button tether

--- hri.py
+++ hri.py
@@ -7,9 +7,16 @@ class Ui(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Load UI
         uic.loadUi("hri.ui", self)
-
         self.show()
+
+        # Tether buttons to functions
+        self.stop_all.clicked.connect(self.button_click)
+
+
+    def button_click(self):
+        print("Mmm, clickeroo")
 
 
 app = QApplication(sys.argv)
