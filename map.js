5a2bf17bab81847918c192c2352ed358d49f96d7
dapeef <42572235+dapeef@users.noreply.github.com>
2022-12-14 22:43:48 +0000

    Fix script loading order and add test function

--- map.js
+++ map.js
@@ -1,9 +1,7 @@
-// Initialize and add the map
-function initMap() {
-    // The map
-    map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
-      center: {lat: 51.6255863, lng: -2.5121819}, // Near Wantage
-      mapTypeId: 'hybrid'
+
+function add_marker(lat, long) {
+    new google.maps.Marker({
+        position: {lat: lat, lng: long},
+        map: map,
     });
 };
