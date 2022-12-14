f413d74c77b9c470196181beae6e8c64d94ba708
dapeef <42572235+dapeef@users.noreply.github.com>
2022-12-14 22:25:10 +0000

    Change map starting settings
    Now start is field next to house

--- map.js
+++ map.js
@@ -2,7 +2,8 @@
 function initMap() {
     // The map
     map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 9,
-      center: {lat: 51.582039, lng: -1.410231}, // Near Wantage
+      zoom: 16,
+      center: {lat: 51.6255863, lng: -2.5121819}, // Near Wantage
+      mapTypeId: 'hybrid'
     });
 };
