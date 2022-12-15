2271e35f82a4a9441d32c7ecd27c4efc9bfb2da5
dapeef <42572235+dapeef@users.noreply.github.com>
2022-12-15 11:11:01 +0000

    Create functions to draw markers
    Sheep
    Herding drones
    Monitor drones

--- hri.py
+++ hri.py
@@ -19,19 +19,59 @@ class Ui(QMainWindow):
         self.map_placeholder_2.deleteLater()
         self.map_placeholder_3.deleteLater()
 
-        # Tether buttons to functions
-        self.stop_all.clicked.connect(self.button_click)
-
         # Initiate HTML elements for maps
         self.browser = QWebEngineView(self.map_box)
         self.browser.load(QUrl().fromLocalFile(os.path.split(os.path.abspath(__file__))[0] + r'\map.html'))
         self.map_layout.addWidget(self.browser)
+        self.browser.loadFinished.connect(self.onLoadFinished) # Once loaded, connect buttons
 
 
-    def button_click(self):
+    def onLoadFinished(self):
+        # Once map is loaded, connect buttons to functions
+        self.stop_all.clicked.connect(self.buttonClick)
+
+        print("Ready!")
+
+
+    def buttonClick(self):        
         print("Mmm, clickeroo")
 
-        self.browser.page().runJavaScript("add_marker(51.6255863, -2.5121819);", print)
+        self.drawTest()
+
+
+    def drawTest(self):
+        sheep_locations = [
+            [51.6255863, -2.5121819],
+            [51.626060, -2.512327],
+            [51.626045, -2.512716],
+            [51.626151, -2.511915],
+            [51.625848, -2.512039],
+            [51.625616, -2.512513],
+            [51.625737, -2.512117]
+        ]
+
+        herding_drone_locations = [
+            [51.626360, -2.513160],
+            [51.626485, -2.512436],
+            [51.626504, -2.511712]
+        ]
+
+        monitor_drone_locations = [
+            [51.625987, -2.512303]
+        ]
+
+        self.drawSheep(sheep_locations)
+        self.drawHerdingDrones(herding_drone_locations)
+        self.drawMonitorDrones(monitor_drone_locations)
+
+    def drawSheep(self, locations):
+        self.browser.page().runJavaScript("addMarkers(" + str(locations) + ", 'sheep');")
+
+    def drawHerdingDrones(self, locations):
+        self.browser.page().runJavaScript("addMarkers(" + str(locations) + ", 'herding_drones');")
+
+    def drawMonitorDrones(self, locations):
+        self.browser.page().runJavaScript("addMarkers(" + str(locations) + ", 'monitor_drones');")
 
 
 app = QApplication(sys.argv)
--- map.js
+++ map.js
@@ -1,9 +1,54 @@
+let markers = {
+    "sheep": [],
+    "herding_drones": [],
+    "monitor_drones": []
+};
 
-function add_marker(lat, long) {
-    new google.maps.Marker({
+function addMarker(lat, long, colour, size) {
+    let marker = new google.maps.Marker({
         position: {lat: lat, lng: long},
         map: map,
+        icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            fillColor: colour,
+            fillOpacity: 0.6,
+            strokeColor: colour,
+            strokeOpacity: 0.9,
+            strokeWeight: 1,
+            scale: size
+        }
     });
 
-    return "Woo, here's the return value!";
+    return marker;
 };
+
+function addMarkers(locations, type) {
+    // Delete all markers
+    for (let i = 0; i < markers[type].length; i++) {
+        markers[type][i].setMap(null);
+    };
+    markers[type].length = 0;
+
+    // Draw new markers
+    let colour;
+    let size;
+    // Choose colour and size depending on type
+    if (type == 'sheep') {
+        colour = '#FFF';
+        size = 5;
+    } else if (type == 'herding_drones') {
+        colour = '#ff8080';
+        size = 7;
+    } else if (type == 'monitor_drones') {
+        colour = '#8080ff';
+        size = 7;
+    };
+    // Draw markers
+    for (let i = 0; i < locations.length; i++) {
+        markers[type].push(addMarker(locations[i][0], locations[i][1], colour, size));
+    };
+}
+
+function drawArchitecture(params) {
+    // Walls, gates, no fly etc
+}
