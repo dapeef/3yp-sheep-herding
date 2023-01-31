f91904ad3858b4522e37a0e67bba20f3b6457d3d
dapeef <42572235+dapeef@users.noreply.github.com>
2023-01-31 15:44:56 +0000

    Irradicate div by zero error in image processing

--- image_processing.py
+++ image_processing.py
@@ -105,24 +105,26 @@ def getCoordinates(file_name, show_image=False):
   for i in contours:
     if cv2.contourArea(i) > 25:
       filtered_contours.append(i)
-  
-
-  # cnts = cv2.drawContours(image, filtered_contours, -1, (0,255,0), thickness = 5)
-  # fig, ax = plt.subplots(1, figsize=(12,8))
+  #cnts = cv2.drawContours(image, filtered_contours, -1, (0,255,0), thickness = 5)
+  #fig, ax = plt.subplots(1, figsize=(12,8))
 
   coordinates = []
 # loop over the contours
-  for c in contours:
-    # compute the center of the contour
-    M = cv2.moments(c)
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    # draw the contour and center of the shape on the image
-    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-    cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-    cv2.putText(image, "center", (cX - 20, cY - 20),
-    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    coordinates.append([cX,cY])
+  for c in filtered_contours:
+    try:
+      # compute the center of the contour
+      M = cv2.moments(c)
+      
+      cX = int(M["m10"] / M["m00"])
+      cY = int(M["m01"] / M["m00"])
+      # draw the contour and center of the shape on the image
+      cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+      cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
+      #cv2.putText(image, "center", (cX - 20, cY - 20),
+      #cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+      coordinates.append([cX,cY])
+    except ZeroDivisionError:
+      pass
   
   # show the image
   if show_image == True:
