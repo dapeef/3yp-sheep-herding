d27c7461795a629c0d6ff6d5014955567def909e
dapeef <42572235+dapeef@users.noreply.github.com>
2023-02-16 15:47:30 +0000

    Create boids demo script

---
+++ boids-demo.py
@@ -0,0 +1,20 @@
+import boids
+import pygame as pg
+
+# Initiate simulation
+sim = boids.Simulation(
+    num_fears=2,
+    num_boids=50,
+    mouse_fear=True,
+    window_size=pg.Vector2(1000, 700)
+)
+
+# Add some predefined walls
+sim.addTestWalls()
+
+# Change one of the fear position's target position
+sim.data.fears[1] = pg.Vector2(950, 650)
+sim.data.fear_targets[1] = pg.Vector2(50, 50)
+
+# Set the simulation running
+sim.mainloop()
