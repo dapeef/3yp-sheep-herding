dfffde6cd0e3f197681a395522cd17dc458993fb
dapeef <42572235+dapeef@users.noreply.github.com>
2023-03-14 15:22:24 +0000

    Update boids-validation.py

--- boids-validation.py
+++ boids-validation.py
@@ -1,14 +1,17 @@
-import pygame as pg
 import boids
+import pygame as pg
 
 # Initialise simulation
 sim = boids.Simulation(
     num_fears=1,
     num_boids=50,
     mouse_fear=True,
-    window_size=pg.Vector2(1000, 700)
+    window_size=pg.Vector2(1500, 900),
+    spawn_zone=pg.Rect(250,250,100,100)
 )
 
 sim.addTestWalls(add_gate=False)
+
+# sim.data.fear_targets[1] = pg.Vector2(200,200)
 
 sim.mainloop()
