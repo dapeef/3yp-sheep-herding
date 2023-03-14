fdbd07a48600834fb4c9748845981c004d4cf446
dapeef <42572235+dapeef@users.noreply.github.com>
2023-03-14 11:44:37 +0000

    Create boids-validation.py

---
+++ boids-validation.py
@@ -0,0 +1,14 @@
+import pygame as pg
+import boids
+
+# Initialise simulation
+sim = boids.Simulation(
+    num_fears=1,
+    num_boids=50,
+    mouse_fear=True,
+    window_size=pg.Vector2(1000, 700)
+)
+
+sim.addTestWalls(add_gate=False)
+
+sim.mainloop()
